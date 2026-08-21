083e6bf0f33eba993f8726f72a3d69a51d2efa25
matricarin <matricarin@gmail.com>
2026-08-21 07:27:37 +0500

    working with db draft

---
+++ src/CoffeeShop.Api/Data/CoffeeShopDbContext.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace CoffeeShop.Api.Data
+{
+    public sealed class CoffeeShopDbContext : IdentityDbContext
+    {
+        //  TODO implement DbContext
+    }
+}
---
+++ src/CoffeeShop.Api/Extensions/ServicesExtensions.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Api.Extensions
+{
+    public static class ServicesExtensions
+    {
+        public static void AddCoffeShopDbContext
+        (
+            this IServiceCollection collection, 
+            IConfiguration configuration
+        )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

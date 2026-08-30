9747223cd7f174f2baf8f241183a75bdf2ab90ba
matricarin <matricarin@gmail.com>
2026-08-30 19:16:46 +0500

    refactored

--- src/frontend/CoffeeShop.Web/Program.cs
+++ src/frontend/CoffeeShop.Web/Program.cs
@@ -9,9 +9,15 @@ public class Program
     {
         var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<App>
+        (
+            "#app"
+        );
 
-        builder.RootComponents.Add<HeadOutlet>("head::after");
+        builder.RootComponents.Add<HeadOutlet>
+        (
+            "head::after"
+        );
 
         builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 

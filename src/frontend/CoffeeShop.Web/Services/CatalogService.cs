389345b62664824734689dae40c061fa3589972a
matricarin <matricarin@gmail.com>
2026-08-31 06:41:44 +0500

    not implemented

---
+++ src/frontend/CoffeeShop.Web/Services/CatalogService.cs
@@ -0,0 +1,16 @@
+﻿namespace CoffeeShop.Web.Services;
+
+public sealed class CatalogService
+{
+    private readonly HttpClient _httpClient;
+
+    public CatalogService(HttpClient httpClient)
+    {
+        _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+    }
+
+    public Task<object> GetCatalogItems()
+    {
+        throw new NotImplementedException();
+    }
+}

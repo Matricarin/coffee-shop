183cb7a9724d06144ebe2d59e16df92ecc6ef265
matricarin <matricarin@gmail.com>
2026-08-31 21:51:26 +0500

    service with stubs were added

--- src/frontend/CoffeeShop.Web/Services/CatalogService.cs
+++ src/frontend/CoffeeShop.Web/Services/CatalogService.cs
@@ -1,16 +1,47 @@
-﻿namespace CoffeeShop.Web.Services;
+﻿using CoffeeShop.Web.Models;
+
+namespace CoffeeShop.Web.Services;
 
 public sealed class CatalogService
 {
-    private readonly HttpClient _httpClient;
-
-    public CatalogService(HttpClient httpClient)
+    public async Task<List<CatalogItem>> GetCatalogItemsAsync()
     {
-        _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
-    }
+        await Task.Delay(300);
 
-    public Task<object> GetCatalogItems()
-    {
-        throw new NotImplementedException();
+        return
+        [
+            new CatalogItem
+            (
+                Guid.NewGuid(),
+                "Espresso",
+                "Black, tasty and hot",
+                1.25m,
+                "/images/espresso.jpg"
+            ),
+            new CatalogItem
+            (
+                Guid.NewGuid(),
+                "Cappuccino",
+                "Milky, fancy and soft",
+                2.00m,
+                "/images/cappuccino.jpg"
+            ),
+            new CatalogItem
+            (
+                Guid.NewGuid(),
+                "Flat White",
+                "Milky, fancy and strong",
+                1.80m,
+                "/images/flat-white.jpg"
+            ),
+            new CatalogItem
+            (
+                Guid.NewGuid(),
+                "Latte",
+                "Milky, chilly and creamy",
+                2.50m,
+                "/images/latte.jpg"
+            ),
+        ];
     }
 }

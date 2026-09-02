d02e5eba8b63f6d782e2ebcdd9c0edaee17b6bc8
matricarin <matricarin@gmail.com>
2026-09-02 22:21:25 +0500

    переименованно

---
+++ src/backend/CoffeeShop.Api/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeShop.Api.Models;
+
+public sealed class ApplicationUser : IdentityUser<Guid>
+{
+    [Required] public string Name { get; init; } = null!;
+    [Required] public DateTime CreatedDate { get; set; } = DateTime.Now;   
+}

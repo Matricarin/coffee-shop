6b92d9ffb7aa308cbda7acf868e2559574ff329f
matricarin <matricarin@gmail.com>
2026-09-03 05:38:46 +0500

    добавлены модели для работы с пользователями

--- src/backend/CoffeeShop.Api/Models/ApplicationUser.cs
+++ src/backend/CoffeeShop.Api/Models/ApplicationUser.cs
@@ -1,10 +1,42 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System.ComponentModel.DataAnnotations;
 
 namespace CoffeeShop.Api.Models;
 
-public sealed class ApplicationUser : IdentityUser<Guid>
+public abstract class ApplicationUser : IdentityUser<Guid>
 {
-    [Required] public string Name { get; init; } = null!;
-    [Required] public DateTime CreatedDate { get; set; } = DateTime.Now;   
+    public DateTime CreatedDate { get; init; } = DateTime.Now;
+    public bool IsActive { get; init; } = true;
+}
+
+public sealed class AdminUser : ApplicationUser
+{
+    public string? ImageUrl { get; set; }
+}
+
+public sealed class ClientUser : ApplicationUser
+{
+    public string? ImageUrl { get; set; }
+}
+
+public static class ApplicationRoles
+{
+    public const string Admin = "admin";
+    public const string Client = "client";
+    public static readonly string[] AvailableRoles = [Admin, Client];
+}
+
+public sealed class ApplicationRole : IdentityRole<Guid>
+{
+    public ApplicationRole(string roleName) : base(roleName)
+    {
+        if (string.IsNullOrWhiteSpace(roleName))
+        {
+            throw new ArgumentNullException(nameof(roleName));
+        }
+
+        if (!ApplicationRoles.AvailableRoles.Contains(roleName))
+        {
+            throw new ArgumentException("Неизвестная роль", nameof(roleName));
+        }
+    }
 }

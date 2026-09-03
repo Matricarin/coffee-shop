f2079233fc94abd67a607ab3edba08e29a3f669b
matricarin <matricarin@gmail.com>
2026-09-03 05:39:45 +0500

    добавлена расширения для регистрации

---
+++ src/backend/CoffeeShop.Api/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,35 @@
+﻿using CoffeeShop.Api.Infrastructure;
+using CoffeeShop.Api.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeShop.Api.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddCoffeeShopContext(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<CoffeeShopContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("PostgresConnection"));
+        });
+
+        return services;
+    }
+
+    public static IServiceCollection AddIdentityContext(this IServiceCollection services)
+    {
+        services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 8;
+
+                options.User.RequireUniqueEmail = false;
+            })
+            .AddEntityFrameworkStores<CoffeeShopContext>()
+            .AddUserManager<UserManager<ApplicationUser>>();
+
+        return services;
+    }
+}

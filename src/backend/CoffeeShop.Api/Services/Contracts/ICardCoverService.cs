11984423d0fb96ebd5693d903f87a2d84a5aac97
matricarin <matricarin@gmail.com>
2026-09-06 08:26:27 +0500

    расширение функциональности сервиса

--- src/backend/CoffeeShop.Api/Services/Contracts/ICardCoverService.cs
+++ src/backend/CoffeeShop.Api/Services/Contracts/ICardCoverService.cs
@@ -8,6 +8,8 @@ public interface ICardCoverService
     CardCover CreateCover();
     CardCover UpdateCover(Guid id);
     void DeleteCover(Guid id);
-    List<CardCover> GetCoverList();
-    CardCover GetCover(Guid id);
+    List<CardCover> GetCoverInfoList();
+    CardCover GetCoverInfo(Guid id);
+    object GetCover(Guid id);
+    List<object> GetCoverList(List<Guid> id);
 }

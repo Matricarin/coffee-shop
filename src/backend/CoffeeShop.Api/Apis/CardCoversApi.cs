a28d49cd6d7e53ed8ed315427e3bd2e31bba4f41
matricarin <matricarin@gmail.com>
2026-09-06 08:26:47 +0500

    добавлены новые эндпойнты

--- src/backend/CoffeeShop.Api/Apis/CardCoversApi.cs
+++ src/backend/CoffeeShop.Api/Apis/CardCoversApi.cs
@@ -1,4 +1,10 @@
-﻿namespace CoffeeShop.Api.Apis;
+﻿using CoffeeShop.Api.Models.Dtos.Response;
+using CoffeeShop.Api.Services.Contracts;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+
+namespace CoffeeShop.Api.Apis;
 
 public static class CardCoversApi
 {
@@ -7,8 +13,24 @@ public static class CardCoversApi
         group.MapPost("/new-cover", () => "Админ добавляет новую обложку");
         group.MapPut("/{id}", () => "Админ редактирует обложку или добавляет новую");
         group.MapGet("/list", () => "Получаем список обложек");
-        group.MapGet("/{id}", (Guid id) => "Получаем выбранную обложку");
+        group.MapGet("/{coverId:guid}", GetCoverInfoById);
+        group.MapGet("/files", () => "Получаем набор файлов карточек");
+        group.MapGet("/file", () => "Получаем файл каорточки");
         group.MapDelete("/{id}", (Guid id) => "Админ удаляет обложку");
         return group;
+    }
+
+    [ProducesResponseType<ProblemDetails>(StatusCodes.Status400BadRequest,
+        "application/problem+json")]
+    private static Task<Results<Ok<CardCoverInfoDto>, NotFound, BadRequest<ProblemDetails>>>
+        GetCoverInfoById
+        (
+            HttpContext context,
+            [AsParameters] ICardCoverService service,
+            [Description("Id обложки для карточки")]
+            Guid coverId
+        )
+    {
+        throw new NotImplementedException();
     }
 }

d2d45648c58af44f5ea16ece38dfd546013ef6b6
matricarin <matricarin@gmail.com>
2026-09-07 05:42:32 +0500

    применение конфигурации

--- src/backend/CoffeeShop.Api/Infrastructure/CoffeeShopContext.cs
+++ src/backend/CoffeeShop.Api/Infrastructure/CoffeeShopContext.cs
@@ -1,4 +1,5 @@
-﻿using CoffeeShop.Api.Models.Entities;
+﻿    using CoffeeShop.Api.Infrastructure.EntityConfigurations;
+    using CoffeeShop.Api.Models.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace CoffeeShop.Api.Infrastructure;
@@ -6,4 +7,9 @@ namespace CoffeeShop.Api.Infrastructure;
 public sealed class CoffeeShopContext : DbContext   
 {
     public DbSet<CardCover> CardCovers { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new CardCoverEntityConfiguration());
+    }
 }

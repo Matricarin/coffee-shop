6922e221971cffb9a301e143edc1e76e253b3d4e
matricarin <matricarin@gmail.com>
2026-09-07 16:56:52 +0500

    удален healthcheck

--- src/backend/CoffeeShop.Api/Program.cs
+++ src/backend/CoffeeShop.Api/Program.cs
@@ -10,26 +10,9 @@ public static class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        //builder.Services.AddCoffeeShopContext(builder.Configuration);
-
-        //builder.Services.AddIdentityContext();
-
-        //  TODO: Зачем нужен? На каком этапе?
-        builder.Services.AddHealthChecks();
-
         builder.Services.AddAuthorization();
         
         var app = builder.Build();
-
-        //  TODO: Зачем он нужен и на каком этапе?
-
-        app.MapHealthChecks("/health", new HealthCheckOptions()
-        {
-            ResponseWriter = async (context, health) =>
-            {
-                await context.Response.WriteAsync("healthy!");
-            }
-        });
         
         app.UseHttpsRedirection();
 

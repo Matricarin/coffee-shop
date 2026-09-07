09e2f527f15a9170518500009e551bf0dc57bde7
matricarin <matricarin@gmail.com>
2026-09-07 05:39:35 +0500

    настройка конфигурации для обложки

--- src/backend/CoffeeShop.Api/Infrastructure/CoffeeShopContext.cs
+++ src/backend/CoffeeShop.Api/Infrastructure/CoffeeShopContext.cs
@@ -1,16 +1,9 @@
-﻿using CoffeeShop.Api.Models;
-using CoffeeShop.Api.Models.Entities;
+﻿using CoffeeShop.Api.Models.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace CoffeeShop.Api.Infrastructure;
 
 public sealed class CoffeeShopContext : DbContext   
 {
-    public DbSet<Purchase> Purchases { get; set; }
-
-    public DbSet<Card> Cards { get; set; }
-
     public DbSet<CardCover> CardCovers { get; set; }
-
-    public DbSet<Offer> Offers { get; set; }
 }

4d65fe3bba34e7a03ef2cf4b7ed75d6dbb77c046
matricarin <matricarin@gmail.com>
2026-09-09 10:47:09 +0500

    добавить пакет для community toolkit

--- src/frontend/CoffeeShop.Admin/ViewModels/ViewModel.cs
+++ src/frontend/CoffeeShop.Admin/ViewModels/ViewModel.cs
@@ -1,6 +1,12 @@
 ﻿namespace CoffeeShop.Admin.ViewModels;
 
-public class ViewModel
+public abstract class ViewModel
 {
-    
+    private bool _isBusy;
+
+    private string? _errorMessage;
+
+    public bool HasError => !string.IsNullOrWhiteSpace(_errorMessage);
+
+    protected void ClearError() => ErrorMessage = null;
 }

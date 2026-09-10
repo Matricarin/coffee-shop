29e46ce856acc66e2e62b4b98cc2320d995874fb
matricarin <matricarin@gmail.com>
2026-09-10 21:41:31 +0500

    корректировка

--- src/frontend/CoffeeShop.Admin/Services/AuthenticationService.cs
+++ src/frontend/CoffeeShop.Admin/Services/AuthenticationService.cs
@@ -1,16 +1,14 @@
-﻿using CoffeeShop.Admin.Models;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace CoffeeShop.Admin.Services;
 
 public interface IAuthenticationService
 {
     Task RegisterAsync(string name, string email, string password);
-    Task<AdminUser> LoginAsync(string email, string password);
+    Task LoginAsync(string email, string password);
     Task LogoutAsync();
 }
 
 public sealed class AuthenticationService
 {
-    
 }

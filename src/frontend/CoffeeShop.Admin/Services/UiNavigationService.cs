b02941d3f9e7b0c1adb8ec1bcda5c76df6d18d2a
matricarin <matricarin@gmail.com>
2026-09-10 15:17:45 +0500

    добавлены основные сущности для аутентификации и контракты с сервисами

---
+++ src/frontend/CoffeeShop.Admin/Services/UiNavigationService.cs
@@ -0,0 +1,18 @@
+﻿using CoffeeShop.Admin.ViewModels;
+using System;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Admin.Services;
+
+public interface IUiNavigationService
+{
+    Task NavigateToAsync<T>() where T : ViewModel;
+}
+
+public sealed class UiNavigationService : IUiNavigationService
+{
+    public Task NavigateToAsync<T>() where T : ViewModel
+    {
+        throw new NotImplementedException();
+    }
+}

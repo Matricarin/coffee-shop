0b965298d7433bdf3d3b19f60168636559580988
matricarin <matricarin@gmail.com>
2026-09-13 09:08:07 +0500

    имплементация аутентификации админа

---
+++ src/frontend/CoffeeShop.Admin/Models/Dtos.cs
@@ -0,0 +1,5 @@
+﻿namespace CoffeeShop.Admin.Models;
+
+public sealed record RegisterDto(string Name, string Email, string Password);
+
+public sealed record LoginDto(string Email, string Password);
--- src/frontend/CoffeeShop.Admin/Services/AuthenticationService.cs
+++ src/frontend/CoffeeShop.Admin/Services/AuthenticationService.cs
@@ -1,31 +1,46 @@
-﻿using System;
+﻿using CoffeeShop.Admin.Models;
+using System;
 using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace CoffeeShop.Admin.Services;
 
 public interface IAuthenticationService
 {
-    Task RegisterAsync(string name, string email, string password);
-    Task LoginAsync(string email, string password);
-    Task LogoutAsync();
+    Task<bool> RegisterAsync(string name, string email, string password);
+    Task<bool> LoginAsync(string email, string password);
+    Task<bool> LogoutAsync();
 }
 
 public sealed class AuthenticationService : IAuthenticationService
 {
     private readonly HttpClient _api;
 
-    public Task RegisterAsync(string name, string email, string password)
+    public async Task<bool> RegisterAsync(string name, string email, string password)
     {
-        throw new NotImplementedException();
+        var data = new RegisterDto(name, email, password);
+
+        var json = JsonSerializer.Serialize(data);
+
+        HttpResponseMessage result = await _api.PostAsJsonAsync("api/admin-auth/register", json);
+
+        return result.IsSuccessStatusCode;
     }
 
-    public Task LoginAsync(string email, string password)
+    public async Task<bool> LoginAsync(string email, string password)
     {
-        throw new NotImplementedException();
+        var data = new LoginDto(email, password);
+
+        var json = JsonSerializer.Serialize(data);
+
+        var result = await _api.PostAsJsonAsync("api/admin-auth/login", json);
+
+        return result.IsSuccessStatusCode;
     }
 
-    public Task LogoutAsync()
+    public Task<bool> LogoutAsync()
     {
         throw new NotImplementedException();
     }
